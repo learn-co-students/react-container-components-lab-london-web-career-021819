5d7d7ff90908637f7e406228e0c1e4657be03b4d
saqemlas <saqemlas@googlemail.com>
2019-04-27 19:07:43 +0100

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const displayReview = ({headline}) => {
+  return <p className="review">{headline}</p>
+}
+
+const MovieReviews = props => {
+  return (
+    <div className="review-list">
+      {props.reviews.map(displayReview)}
+    </div>)
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,48 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const NYT_API_KEY = 'AssJeB3es2zr4vZEsPdI2d1zrGDpNkqY';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+  state = {
+    reviews: [],
+    searchTerm: null
+    }
+
+componentDidMount(){
+  fetch(URL)
+  .then(resp => resp.json())
+  .then(resp => {
+      this.setState({
+          reviews: resp.results})
+} ) }
+
+search = event => {
+  event.preventDefault();
+  fetch(URL+`&query=${this.state.searchTerm}`)
+      .then(resp => resp.json())
+      .then(resp => {
+          this.setState({
+              reviews: resp.results})
+} ) }
+
+handleChange = e => {
+  this.setState({
+    searchTerm: e.target.value})
+}
+
+render(){
+  return (
+    <div className="searchable-movie-reviews">
+      <form onSubmit={this.search}>
+        <input name="query" onChange={this.handleChange} value={this.state.searchTerm} />
+        <input type="submit" />
+      </form>
+          <MovieReviews reviews={this.state.reviews} />
+    </div>
+) }
+
+}
